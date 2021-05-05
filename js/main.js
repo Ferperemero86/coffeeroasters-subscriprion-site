27a860f597631d92202ca5749020283fb82c99a2
Ferperemero86 <fernandoperezmerono@gmail.com>
2021-05-05 18:27:39 +0100

    Create menu-selection

--- js/main.js
+++ js/main.js
@@ -1,149 +1,109 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Menu bar
-  const menuBar = document.querySelector('.menu-bar');
-
-  document.addEventListener('scroll', function(e) {
-    if (window.pageYOffset > menuBar.offsetTop) {
-      menuBar.classList.add('fixed');
-
-    } else {
-      menuBar.classList.remove('fixed');
-    }
-  })
-
-  // Main menu
-  const hamburger = document.querySelector('.hamburger');
-  const closeIcon = document.querySelector('.close-icon');
-  const verticalMenu = document.querySelector('.m-vertical-menu');
-
-  hamburger.addEventListener('click', function() {
-    verticalMenu.classList.remove('slide-up');
-    verticalMenu.classList.add('slide-down');
-    hamburger.classList.add('hidden');
-    closeIcon.classList.remove('hidden');
-  });
-
-  closeIcon.addEventListener('click', function() {
-    verticalMenu.classList.add('slide-up');
-    verticalMenu.classList.remove('slide-down');
-    hamburger.classList.remove('hidden');
-    closeIcon.classList.add('hidden');
-  })
-
-
-  // Projects slider
-  const bodyContentBtn = document.querySelector('.o-projects .body-content-btn');
-  const headerImg = document.querySelectorAll('.o-projects-header img');
-  const textHeading = document.querySelector('.o-projects .header-text-heading');
-  const text = document.querySelector('.o-projects .header-text-content');
-
-
-  let currentIndex = 0;
-  const images = ['first', 'second', 'third'];
-  const imageUrls = {
-
-    'first' : {
-      'images':
-        [
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469176/creative-site/mobile/monirn1ybzxr31mh0osd.jpg',
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469021/creative-site/desktop/nyzwb1qbbsijvj8otp1y.jpg',
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469021/creative-site/desktop/nyzwb1qbbsijvj8otp1y.jpg'
-        ],
-      'text': {
-        'heading': 'Learn Product Roadmap',
-        'content': '2019 Project'
-      }
-
-    },
-    'second' : {
-      'images':
-        [
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469177/creative-site/mobile/barvrsdva3ilaovzoi0k.jpg',
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469271/creative-site/tablet/hgxxol6lrquoniod1hwp.jpg',
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469023/creative-site/desktop/ygcdewlzhl4c5ayxqqww.jpg'
-        ],
-      'text': {
-        'heading': 'New Majestic Hotel',
-        'content': '2018 Project'
-      }
-    },
-    'third' : {
-      'images':
-        [
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469177/creative-site/mobile/barvrsdva3ilaovzoi0k.jpg',
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469021/creative-site/desktop/kllwr8p1ewu9ag259il5.jpg',
-          'https://res.cloudinary.com/dby4kdmbv/image/upload/v1617469021/creative-site/desktop/kllwr8p1ewu9ag259il5.jpg'
-
-        ],
-      'text': {
-        'heading': 'Crypto Dashboard',
-        'content': '2016 Project'
-      }
-    }
-
+  //// Menu selection ////
+  const selectMenu = document.querySelector('.o-selection-menu');
+  const orderSumary = {
+    'how': 'Capsule',
+    'type': 'Single Origin',
+    'quantity': '250g',
+    'grind': 'Wholebean',
+    'delivery': 'Every Week'
   };
 
+  const getCardSelectedText = function (cardEl) {
+    let card = cardEl;
+    let found = false;
+    let result = 'test';
 
-  function addClasses(classVal) {
-    for (let i=0; i < headerImg.length; i++) {
-      headerImg[i].classList.add(classVal);
+    while (!found) {
+
+      if (card.children[0].classList.contains('a-heading-3')) {
+        found = true;
+        result = card.children[0].innerHTML;
+
+      } else {
+        card = card.children[0];
+      }
     }
+
+    return result;
+
   }
 
-  function removeClasses(classVal) {
-    for (let i=0; i < headerImg.length; i++) {
-      headerImg[i].classList.remove(classVal);
-    }
-  }
-
-  function addUrls(currentUrlImages) {
-    for (let i=0; i<headerImg.length; i++) {
-      headerImg[i].src = currentUrlImages[i];
-    }
-
-    setTimeout(function(){
-      addClasses('slide-in');
-      removeClasses('slide-out-left');
-    }, 10); 
-
-   const currentText = imageUrls[images[currentIndex]]['text'];
   
-   textHeading.innerHTML = currentText['heading'];
-   text.innerHTML = currentText['content'];
+  // Card display
+  const removeClasses = function(cards) {
+
+    for (let i=0; i<cards.length; i++) {
+      if (cards[i].classList.contains('card-active')) {
+        cards[i].classList.remove('card-active');
+      }
+    }
   }
 
- 
-  bodyContentBtn.addEventListener('click', function(e) {
-    const btn = e.target;
-
-    removeClasses('slide-in');
-    addClasses('slide-out-left');
-
-    if (btn.classList.contains('left')) {
-
-      if (currentIndex === 0) {
-        currentIndex = images.length-1;
-
-      } else {
-        currentIndex--;
-      }
-     
-
-    } else if (btn.classList.contains('right')) {
-      
-      if (currentIndex === images.length-1) {
-        currentIndex = 0;
-
-      } else {
-        currentIndex++;
-      }
-
+  const addSelectionToOrder = function(cardHeading) {
+    if (cardHeading === 'Capsule' || cardHeading === 'Filter' || cardHeading === 'Espresso') {
+      orderSumary['how'] = cardHeading;
     }
 
-    const currentUrlImages = imageUrls[images[currentIndex]]['images'];
-    
-    addUrls(currentUrlImages);
+    if (cardHeading === 'Single Origin' || cardHeading === 'Decaf' || cardHeading === 'Blended') {
+      orderSumary['type'] = cardHeading;
+    }
 
-  });
+    if (cardHeading === '250g' || cardHeading === '500g' || cardHeading === '1000g') {
+      orderSumary['quantity'] = cardHeading;
+    }
+
+    if (cardHeading === 'Wholebean' || cardHeading === 'Filter' || cardHeading === 'Cafetiére') {
+      orderSumary['grind'] = cardHeading;
+    }
+
+    if (cardHeading === 'Every week' || cardHeading === 'Every 2 weeks' || cardHeading === 'Every month') {
+      orderSumary['delivery'] = cardHeading;
+    }
+  }
+
+
+  for (let i=0; i<selectMenu.children.length; i++) {
+    const currentMenu = selectMenu.children[i];
+    const currentMenuBody = currentMenu.children[1];
+    const arrow = currentMenu.children[0].children[1];
+    const cards = currentMenu.children[1];
+
+
+    arrow.addEventListener('click', function(e) {
+      
+      if (currentMenuBody.classList.contains('slide-up')) {
+        currentMenuBody.classList.remove('slide-up');
+        currentMenuBody.classList.add('slide-down');
+        this.classList.remove('a-arrow-right');
+        this.classList.add('a-arrow-down');
+
+      } else {
+        currentMenuBody.classList.remove('slide-down');
+        currentMenuBody.classList.add('slide-up');
+        this.classList.remove('a-arrow-down');
+        this.classList.add('a-arrow-right');
+
+      }
+      
+    });
+
+    cards.addEventListener('click', function(e) {
+      const card = e.target.closest('.menu-field-card');
+      const cardsChildren = this.children;
+
+      removeClasses(cardsChildren);
+
+      if (!card.classList.contains('card-active')) {
+        card.classList.add('card-active');
+        cardSelectedText = getCardSelectedText(card);
+        
+        addSelectionToOrder(cardSelectedText);
+        console.log(orderSumary);
+      }
+    })
+    
+  }
+
 });
